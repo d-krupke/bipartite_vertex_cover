219c9763310d2b359452ffb44127c847c2d290ee
dkrupke <d.krupke@tu-bs.de>
2017-10-11 17:59:50 +0200

    Wrong implementation of adding vertices that are connected by an alternating path. Hopefully fixed now.

--- details/VertexCoverSolver.h
+++ details/VertexCoverSolver.h
@@ -10,91 +10,81 @@
 #include "../Partition.h"
 #include "MatchingSolver.h"
 
-namespace bipartvc
-{
-  namespace details
-  {
-    template<typename G, typename F>
-    class VertexCoverSolver
-    {
-    public:
-      VertexCoverSolver(const G &graph, F partition_classifier) : graph{&graph},
-                                                                  partition_classifier{partition_classifier}
-      {
-        in_vertex_cover.resize(boost::num_vertices(graph), false);
-      }
-
-      bool
-      solve()
-      {
-        details::MatchingSolver<G> matchingSolver{*graph};
-        matchingSolver.solve();
-        add_necessary_vertices_of_partition_a(matchingSolver);
-        add_remaining_necessary_vertices_from_partition_b(matchingSolver);
-        return true;
-      }
-
-      bool
-      isInVertexCover(Vertex<G> v)
-      {
-        const auto idx = boost::get(boost::vertex_index, *graph, v);
-        return in_vertex_cover.at(idx);
-      }
-
-    private:
-      void
-      add_necessary_vertices_of_partition_a(const details::MatchingSolver<G> &matchingSolver)
-      {
-        for (const auto &v: boost::make_iterator_range(boost::vertices(*graph))) {
-          if (partition_classifier(v) == Partition::A) {
-            if (matchingSolver.isMatched(v)) {
-              for (const auto &n: make_iterator_range(boost::adjacent_vertices(v, *graph))) {
-                if (!matchingSolver.isMatched(n)) {
-                  recursive_add(v);
-                }
-              }
-            }
-          }
-        }
-      }
-
-      void
-      add_remaining_necessary_vertices_from_partition_b(const details::MatchingSolver<G> &matchingSolver)
-      {
-        for (const auto &v: boost::make_iterator_range(boost::vertices(*graph))) {
-          if (partition_classifier(v) == Partition::B) {
-            if (matchingSolver.isMatched(v)) {
-              if (!in_vertex_cover.at(matchingSolver.getPartner(v))) {
-                const auto idx = boost::get(boost::vertex_index, *graph, v);
-                in_vertex_cover[idx] = true;
-                size+=1;
-              }
-            }
-          }
-        }
-      }
-
-      void
-      recursive_add(Vertex<G> v)
-      {
-        const auto idx = boost::get(boost::vertex_index, *graph, v);
-        if (in_vertex_cover.at(idx)) { return; }
-        in_vertex_cover.at(idx) = true;
-        size+=1;
-        for (const auto &n: boost::make_iterator_range(boost::adjacent_vertices(v, *graph))) {
-          for (const auto &nn: boost::make_iterator_range(boost::adjacent_vertices(n, *graph))) {
-            recursive_add(nn);
-          }
-        }
-      }
-
-      const G *graph;
-      F partition_classifier;
-      std::vector<bool> in_vertex_cover;
-      size_t size=0;
-    };
+namespace bipartvc {
+namespace details {
+template<typename G, typename F>
+class VertexCoverSolver {
+ public:
+  VertexCoverSolver(const G &graph, F partition_classifier) : graph{&graph},
+                                                              partition_classifier{partition_classifier} {
+    in_vertex_cover.resize(boost::num_vertices(graph), false);
   }
-}
 
+  bool
+  solve() {
+    details::MatchingSolver<G> matchingSolver{*graph};
+    matchingSolver.solve();
+    add_necessary_vertices_of_partition_a(matchingSolver);
+    add_remaining_necessary_vertices_from_partition_b(matchingSolver);
+    return true;
+  }
+
+  bool
+  isInVertexCover(Vertex<G> v) {
+    const auto idx = boost::get(boost::vertex_index, *graph, v);
+    return in_vertex_cover.at(idx);
+  }
+
+ private:
+  void
+  add_necessary_vertices_of_partition_a(const details::MatchingSolver<G> &matchingSolver) {
+    for (const auto &v: boost::make_iterator_range(boost::vertices(*graph))) {
+      if (partition_classifier(v) == Partition::A) {
+        if (matchingSolver.isMatched(v)) {
+          for (const auto &n: make_iterator_range(boost::adjacent_vertices(v, *graph))) {
+            if (!matchingSolver.isMatched(n)) {
+              recursive_add(v, matchingSolver);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  void
+  add_remaining_necessary_vertices_from_partition_b(const details::MatchingSolver<G> &matchingSolver) {
+    for (const auto &v: boost::make_iterator_range(boost::vertices(*graph))) {
+      if (partition_classifier(v) == Partition::B) {
+        if (matchingSolver.isMatched(v)) {
+          if (!in_vertex_cover.at(matchingSolver.getPartner(v))) {
+            const auto idx = boost::get(boost::vertex_index, *graph, v);
+            in_vertex_cover[idx] = true;
+            size += 1;
+          }
+        }
+      }
+    }
+  }
+
+  void
+  recursive_add(Vertex<G> v, const details::MatchingSolver<G> &matchingSolver) {
+    const auto idx = boost::get(boost::vertex_index, *graph, v);
+    if (in_vertex_cover.at(idx)) { return; }
+    in_vertex_cover.at(idx) = true;
+    size += 1;
+    auto n = matchingSolver.getPartner(v);
+    for (const auto &nn: boost::make_iterator_range(boost::adjacent_vertices(n, *graph))) {
+      assert(matchingSolver.isMatched(nn));
+      recursive_add(nn, matchingSolver);
+    }
+  }
+
+  const G *graph;
+  F partition_classifier;
+  std::vector<bool> in_vertex_cover;
+  size_t size = 0;
+};
+}
+}
 
 #endif //BIPARTITE_VERTEX_COVER_VERTEXCOVERSOLVER_H
--- min_vertex_cover_gtest.cpp
+++ min_vertex_cover_gtest.cpp
@@ -6,8 +6,7 @@
 #include <boost/graph/adjacency_list.hpp>
 #include "min_vertex_cover.h"
 
-TEST(Library, IsValidVertexCover)
-{
+TEST(Library, IsValidVertexCover) {
   using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS>;
   Graph graph{6};
   boost::add_edge(0, 3, graph);
@@ -32,8 +31,7 @@ TEST(Library, IsValidVertexCover)
   ASSERT_TRUE(bipartvc::is_valid_vertex_cover(graph, vertex_cover_2));
 }
 
-TEST(Library, MinVertexCover)
-{
+TEST(Library, MinVertexCover) {
   using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS>;
   Graph graph{7};
   boost::add_edge(0, 3, graph);
@@ -48,11 +46,11 @@ TEST(Library, MinVertexCover)
   boost::add_edge(2, 6, graph);
 
   auto partition_classifier = [](boost::graph_traits<Graph>::vertex_descriptor v) -> bipartvc::Partition {
-      if (v < 3) {
-        return bipartvc::Partition::A;
-      } else {
-        return bipartvc::Partition::B;
-      }
+    if (v < 3) {
+      return bipartvc::Partition::A;
+    } else {
+      return bipartvc::Partition::B;
+    }
   };
   auto vertex_cover = bipartvc::get_minimal_vertex_cover(graph, partition_classifier);
 
@@ -61,11 +59,11 @@ TEST(Library, MinVertexCover)
 
   //swap partitions (A is treated differently than B by the algorithm so we should check this)
   auto complementary_partition_classifier = [](boost::graph_traits<Graph>::vertex_descriptor v) -> bipartvc::Partition {
-      if (v < 3) {
-        return bipartvc::Partition::B;
-      } else {
-        return bipartvc::Partition::A;
-      }
+    if (v < 3) {
+      return bipartvc::Partition::B;
+    } else {
+      return bipartvc::Partition::A;
+    }
   };
   auto vertex_cover_2 = bipartvc::get_minimal_vertex_cover(graph, complementary_partition_classifier);
 
@@ -77,3 +75,92 @@ TEST(Library, MinVertexCover)
   ASSERT_EQ(vertex_cover_3.size(), 3);
   ASSERT_TRUE(bipartvc::is_valid_vertex_cover(graph, vertex_cover_3));
 }
+
+TEST(MinVertexCover, Star) {
+  using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS>;
+  Graph graph{7};
+  boost::add_edge(0, 2, graph);
+  boost::add_edge(0, 3, graph);
+  boost::add_edge(0, 4, graph);
+  boost::add_edge(0, 5, graph);
+  boost::add_edge(0, 6, graph);
+  boost::add_edge(1, 2, graph);
+  boost::add_edge(1, 3, graph);
+  boost::add_edge(1, 4, graph);
+  boost::add_edge(1, 5, graph);
+  boost::add_edge(1, 6, graph);
+  auto partition_classifier = [](boost::graph_traits<Graph>::vertex_descriptor v) -> bipartvc::Partition {
+    if (v < 2) {
+      return bipartvc::Partition::A;
+    } else {
+      return bipartvc::Partition::B;
+    }
+  };
+  auto vertex_cover = bipartvc::get_minimal_vertex_cover(graph, partition_classifier);
+
+  ASSERT_EQ(vertex_cover.size(), 2);
+  ASSERT_TRUE(bipartvc::is_valid_vertex_cover(graph, vertex_cover));
+
+  //swap partitions (A is treated differently than B by the algorithm so we should check this)
+  auto complementary_partition_classifier = [](boost::graph_traits<Graph>::vertex_descriptor v) -> bipartvc::Partition {
+    if (v < 2) {
+      return bipartvc::Partition::B;
+    } else {
+      return bipartvc::Partition::A;
+    }
+  };
+  auto vertex_cover_2 = bipartvc::get_minimal_vertex_cover(graph, complementary_partition_classifier);
+
+  ASSERT_EQ(vertex_cover_2.size(), 2);
+  ASSERT_TRUE(bipartvc::is_valid_vertex_cover(graph, vertex_cover_2));
+
+  //and with automatic partition detection.
+  auto vertex_cover_3 = bipartvc::get_minimal_vertex_cover(graph);
+  ASSERT_EQ(vertex_cover_3.size(), 2);
+  ASSERT_TRUE(bipartvc::is_valid_vertex_cover(graph, vertex_cover_3));
+
+}
+
+TEST(MinVertexCover, ConnectedStar) {
+  using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS>;
+  Graph graph{12};
+  boost::add_edge(0, 2, graph);
+  boost::add_edge(0, 3, graph);
+  boost::add_edge(0, 4, graph);
+  boost::add_edge(0, 5, graph);
+  boost::add_edge(0, 6, graph);
+  boost::add_edge(1, 7, graph);
+  boost::add_edge(1, 8, graph);
+  boost::add_edge(1, 9, graph);
+  boost::add_edge(1, 10, graph);
+  boost::add_edge(1, 11, graph);
+  boost::add_edge(0, 1, graph);
+
+  auto vertex_cover_3 = bipartvc::get_minimal_vertex_cover(graph);
+  ASSERT_EQ(vertex_cover_3.size(), 2);
+  ASSERT_TRUE(bipartvc::is_valid_vertex_cover(graph, vertex_cover_3));
+
+}
+
+TEST(MinVertexCover, EvenCycle) {
+  using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS>;
+  Graph graph{12};
+  boost::add_edge(0, 1, graph);
+  boost::add_edge(1, 2, graph);
+  boost::add_edge(2, 3, graph);
+  boost::add_edge(3, 4, graph);
+  boost::add_edge(4, 5, graph);
+  boost::add_edge(5, 6, graph);
+  boost::add_edge(6, 7, graph);
+  boost::add_edge(7, 8, graph);
+  boost::add_edge(8, 9, graph);
+  boost::add_edge(9, 10, graph);
+  boost::add_edge(10, 11, graph);
+  boost::add_edge(11, 0, graph);
+
+  auto vertex_cover_3 = bipartvc::get_minimal_vertex_cover(graph);
+  ASSERT_EQ(vertex_cover_3.size(), 6);
+  ASSERT_TRUE(bipartvc::is_valid_vertex_cover(graph, vertex_cover_3));
+
+}
+
